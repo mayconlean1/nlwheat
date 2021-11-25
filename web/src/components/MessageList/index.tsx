6461f4b46de397fd8248defd865662392e4c812d
mayconlean1 <mayconlean@hotmail.com>
2021-11-24 21:52:08 -0300

    create styles and interface in react web

---
+++ web/src/components/MessageList/index.tsx
@@ -0,0 +1,54 @@
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+
+export function MessageList () {
+    return (
+        <div className={styles.messageListWrapper}>
+            <img src={logoImg} alt="DoWhile 2021" />
+
+            <ul className={styles.messageList}>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos. vamooo pra cima!!
+                    </p>
+                    <div className={styles.messageUser} >
+
+                        <div className={styles.userImage} >
+                            <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+                        </div>
+                        <span>Diego Fernandes</span>
+
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos. vamooo pra cima!!
+                    </p>
+                    <div className={styles.messageUser} >
+
+                        <div className={styles.userImage} >
+                            <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+                        </div>
+                        <span>Diego Fernandes</span>
+
+                    </div>
+                </li>
+                <li className={styles.message}>
+                    <p className={styles.messageContent}>
+                        Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos. vamooo pra cima!!
+                    </p>
+                    <div className={styles.messageUser} >
+
+                        <div className={styles.userImage} >
+                            <img src="https://github.com/diego3g.png" alt="Diego Fernandes" />
+                        </div>
+                        <span>Diego Fernandes</span>
+
+                    </div>
+                </li>
+
+            </ul>
+        </div>
+    )
+}
