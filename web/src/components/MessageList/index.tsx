ee0141318318132309db0750c5bbceed268f0732
mayconlean1 <mayconlean@hotmail.com>
2021-11-25 22:17:01 -0300

    add bg when signedup, add socket io and render results in page, fixed unique id in messages map in index MessageList Li key

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 import {api} from '../../services/api'
 
 import styles from './styles.module.scss'
@@ -14,8 +15,30 @@ type Message = {
     }
 }
 
+const messagesQueue : Message[] = []
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', (newMessage: Message)=>{
+    messagesQueue.push(newMessage)
+})
+
 export function MessageList () {
     const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(()=>{
+        const timer = setInterval(()=>{
+            if(messagesQueue.length > 0 ){
+                setMessages( prevState => [
+                    messagesQueue[0],
+                    prevState[0],
+                    prevState[1],
+                ].filter(Boolean))
+
+                messagesQueue.shift()
+            }
+        }, 3000)
+    },[])
 
     useEffect(()=>{
         api.get<Message[]>('messages/last3').then(response =>{
@@ -30,9 +53,9 @@ export function MessageList () {
 
             <ul className={styles.messageList}>
 
-                {messages.map(message =>{
+                {messages.map((message,index) =>{
                     return (
-                        <li key={message.id} className={styles.message}>
+                        <li key={index} className={styles.message}>
                             <p className={styles.messageContent}>
                                 {message.text}
                             </p>
